f692955f884a615daf1026ee2120c0322711c3a5
Larisa N <larisale@gmail.com>
2024-11-12 20:20:22 -0800

    BrowserStack settings

--- features/environment.py
+++ features/environment.py
@@ -3,23 +3,25 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
 
 from app.application import Application
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
-    driver_path = './chromedriver.exe'
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
-
+    # driver_path = './chromedriver.exe'
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+    #
     # driver_path = GeckoDriverManager().install()
     # service = Service(driver_path)
     # context.driver = webdriver.Firefox(service=service)
 
     ### HEADLESS MODE ####
+    #DID NOT WORK FORM ME FOR TASK# 3
     # options = webdriver.ChromeOptions()
     # options.add_argument('headless')
     # service = Service(ChromeDriverManager().install())
@@ -28,15 +30,39 @@ def browser_init(context):
     #     options=options,
     #     service=service
     # )
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver_path = './chromedriver.exe'
-    service = Service(driver_path)
+    #WORKED! FOR TASK# 3
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # driver_path = './chromedriver.exe'
+    # service = Service(driver_path)
+    #
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
 
-    context.driver = webdriver.Chrome(
-        options=options,
-        service=service
-    )
+    ## BROWSERSTACK ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'larisa_t3lePz'
+    bs_key = 'MNfpB7RR7gXKZXYPLsyp'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        # "os": "Windows",
+        # "osVersion" : "11",
+        # 'browserName': 'chrome',
+
+        "os": "OS X",
+        "osVersion" : "ventura",
+        'browserName': 'chrome',
+        'sessionName': scenario_name
+    }
+    #
+    # for Mac: "os": "OS X", "osVersion" : "ventura", 'browserName': 'chrome' 'sessionName': scenario_name
+
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -47,7 +73,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
